778a7d2c27a092118a54739889ec7adf877f727d
Allan Gallo <allan.gallo.guerra@gmail.com>
2023-04-10 18:46:29 -0600

    Initial Configuration for the server

---
+++ README.md
@@ -0,0 +1,23 @@
+# E-Commerce Backend
+
+E-Commerce Backend handles all request for E-Commerce Client.
+
+## Setup
+### Clone the repository
+```bash
+git clone
+```
+### Install dependencies
+Movin to the directory
+```bash
+cd ecommerce-backend
+```
+
+Install dependencies
+```bash
+yarn install
+```
+Or
+```bash
+npm install
+```
---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `
+  type Book {
+    title: String
+    author: String
+    new: Boolean
+  }
+
+  type Query {
+    books: [Book]
+  }
+`
+
+const books = [
+  {
+    title: "The awakeining",
+    author: 'Kate Chopin',
+    new: true
+  },
+  {
+    title: "City of Glass",
+    author: 'Paul Auster',
+    new: false
+  },
+]
+
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  }
+}
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+
+const { url } = await startStandaloneServer(server, { listen: { port: 4000 } })
+
+console.log(`Server listening at: ${url} 🐳`)
